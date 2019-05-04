d4e1b3d4213f5b5b44b09e7966832da38a092a8f
maxep <me@maxep.me>
2019-05-04 12:35:23 +0200

    Upgrade to iOS8 and Xcode 10.2

--- SWHttpTrafficRecorder/SWHttpTrafficRecorder.m
+++ SWHttpTrafficRecorder/SWHttpTrafficRecorder.m
@@ -280,15 +280,6 @@ static NSString * const SWRecordingLProtocolHandledKey = @"SWRecordingLProtocolH
                                                  }];
 }
 
-
-- (void)connection:(NSURLConnection *)connection didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge {
-    [self.client URLProtocol:self didReceiveAuthenticationChallenge:challenge];
-}
-
-- (void)connection:(NSURLConnection *)connection didCancelAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge {
-    [self.client URLProtocol:self didCancelAuthenticationChallenge:challenge];
-}
-
 - (NSURLRequest *)connection:(NSURLConnection *)connection willSendRequest:(NSURLRequest *)request redirectResponse:(NSURLResponse *)response {
     if (response != nil) {
         [[self client] URLProtocol:self wasRedirectedToRequest:request redirectResponse:response];
